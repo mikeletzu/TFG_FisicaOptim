564702abe647b5c8252534df9beb6c9cdc4a307f
mikeletzu <120392721+mikeletzu@users.noreply.github.com>
2026-02-09 12:46:58 +0100

    ball auto

--- PruebaCloth/Assets/Scripts/BallMovement.cs
+++ PruebaCloth/Assets/Scripts/BallMovement.cs
@@ -4,6 +4,10 @@ public class BallMovement : MonoBehaviour
 {
     [SerializeField]
     private float movementSpeed = 10.0f;
+    [SerializeField]
+    private bool isAuto = false;
+    private int dir = -1;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,33 +19,46 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isAuto) AutoUpdate();
+        else KeyUpdate();
+    }
+
+    void KeyUpdate()
+    {
         // Movemos a la bola según el input en tres ejes
-            // X
+        // X
         if (Input.GetKey(KeyCode.D)) // Establecer límites para el movimiento?
         {
-			transform.position += Vector3.right * Time.deltaTime * movementSpeed;
-		}
+            transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+        }
         else if (Input.GetKey(KeyCode.A))
         {
-			transform.position += Vector3.left * Time.deltaTime * movementSpeed;
-		}
-            // Y
+            transform.position += Vector3.left * Time.deltaTime * movementSpeed;
+        }
+        // Y
         if (Input.GetKey(KeyCode.S))
         {
-			transform.position += Vector3.back * Time.deltaTime * movementSpeed;
-		}
+            transform.position += Vector3.back * Time.deltaTime * movementSpeed;
+        }
         else if (Input.GetKey(KeyCode.W))
         {
-			transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
-		}
-            // Z
+            transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+        }
+        // Z
         if (Input.GetKey(KeyCode.R))
         {
-			transform.position += Vector3.up * Time.deltaTime * movementSpeed;
-		}
-		else if (Input.GetKey(KeyCode.F))
-		{
-			transform.position += Vector3.down * Time.deltaTime * movementSpeed;
-		}
-	}
+            transform.position += Vector3.up * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            transform.position += Vector3.down * Time.deltaTime * movementSpeed;
+        }
+    }
+
+    void AutoUpdate()
+    {
+        if (transform.position.x > 4) dir = -1;
+        else if (transform.position.x < -4) dir = 1;
+        transform.position += dir * Vector3.right * Time.deltaTime * movementSpeed;
+    }
 }
