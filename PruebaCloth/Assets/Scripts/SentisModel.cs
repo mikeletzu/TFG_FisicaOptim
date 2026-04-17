7965638b0a099c9ded1ef5c6b8cfdf1fb8651f25
efeliua <100871802+efeliua@users.noreply.github.com>
2026-04-17 11:27:01 +0200

    Denormalización en Unity (still doesnt work :( )
    Quizá los bounds son los amigos que hicimos por el camino. Claramente no va.

--- PruebaCloth/Assets/Scripts/SentisModel.cs
+++ PruebaCloth/Assets/Scripts/SentisModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.IO;
 using Unity.AppUI.UI;
@@ -5,6 +6,7 @@ using Unity.InferenceEngine;
 using UnityEditor.Build.Content;
 using UnityEngine;
 using UnityEngine.UIElements;
+
 
 public class ClothML : MonoBehaviour
 {
@@ -22,7 +24,28 @@ public class ClothML : MonoBehaviour
 
     int vertexCount;
 
-    void Start()
+	// Datos de normalización
+	public TextAsset jsonFile;
+
+	[System.Serializable]
+	public class NormalizationData
+	{
+		public float[] mean;
+		public float[] std;
+		public string[] feature_prefixes;
+	}
+	public NormalizationData normData;
+
+	void Awake()
+	{
+		if (jsonFile != null)
+		{
+			normData = JsonUtility.FromJson<NormalizationData>(jsonFile.text);
+			Debug.Log("Media de "+ normData.feature_prefixes[0].ToString() + ": "+ normData.mean[0].ToString());
+		}
+	}
+
+	void Start()
     {
         var model = ModelLoader.Load(modelAsset);
         worker = new Worker(model, BackendType.GPUCompute);
@@ -32,6 +55,12 @@ public class ClothML : MonoBehaviour
 		vertexCount = clothMeshFilter.mesh.vertexCount;
 
         lastVertexPositions = new Vector3[vertexCount];
+
+		for (int im = 0; im < vertexCount; im++)
+		{
+			lastVertexPositions[im] = new Vector3(0,0,0);
+		}
+
         ballCollider = ball.GetComponent<SphereCollider>();
 
         maxDistance = new float[vertexCount];
@@ -51,7 +80,8 @@ public class ClothML : MonoBehaviour
             maxDistance[i] = 0.2f;
             i++;
         }
-    }
+
+	}
 
 	public void SaveData(string data)
 	{
@@ -60,6 +90,7 @@ public class ClothML : MonoBehaviour
 		File.AppendAllText(filePath, data + "\n");
 	}
 
+	// Versión de antes de correciones
 	//void Update()
 	//   {
 	//       var mesh = clothMeshFilter.mesh;
@@ -142,28 +173,25 @@ public class ClothML : MonoBehaviour
 		var normals = mesh.normals;
 		var uvs = mesh.uv; // Cachear las uvs fuera del bucle
 
-		// 1. Reutilizar o crear tensor
 		inputTensor = new Tensor<float>(new TensorShape(1, vertexCount, 13));
 
 		for (int i = 0; i < vertexCount; i++)
 		{
-			// Sugerencia: Trabaja TODO en Local Space si es posible
 			Vector3 worldPos = clothMeshFilter.transform.TransformPoint(vertices[i]);
 			Debug.DrawRay(worldPos, Vector3.up * 0.1f, UnityEngine.Color.red);
 
-			// CORRECCIÓN VELOCIDAD: (PosActual - PosAnterior) / Time.deltaTime
 			Vector3 vel = (worldPos - lastVertexPositions[i]) / Time.deltaTime;
 
 			float sdf = Vector3.Distance(worldPos, ball.transform.position) - ballCollider.radius;
 			Vector3 normal = normals[i];
 
 			int f = 0;
-			// POSICIÓN (Asegúrate de qué espacio espera tu modelo, aquí asumo World)
+			// Posicion
 			inputTensor[0, i, f++] = worldPos.x;
 			inputTensor[0, i, f++] = worldPos.y;
 			inputTensor[0, i, f++] = worldPos.z;
 
-			// VELOCIDAD REAL
+			// Velocidad
 			inputTensor[0, i, f++] = vel.x;
 			inputTensor[0, i, f++] = vel.y;
 			inputTensor[0, i, f++] = vel.z;
@@ -176,7 +204,7 @@ public class ClothML : MonoBehaviour
 			inputTensor[0, i, f++] = uvs[i].x;
 			inputTensor[0, i, f++] = uvs[i].y;
 
-			// ACTUALIZAR ESTADO PARA EL PRÓXIMO FRAME
+			// Para la velocidad del proximo frame
 			lastVertexPositions[i] = worldPos;
 		}
 
@@ -189,9 +217,34 @@ public class ClothML : MonoBehaviour
 		string vertex = "";
 		for (int i = 0; i < vertexCount; i++)
 		{
-			// Si el modelo devuelve World Space, conviértelo a local antes de aplicar
-			Vector3 modelOutput = new Vector3(result[0, i, 0], result[0, i, 1], result[0, i, 2]);
+			// World space!
+			float normalizedX = result[0, i, 0]; 
+			float normalizedY = result[0, i, 1]; 
+			float normalizedZ = result[0, i, 2];
+
+			// El que tendria que ser
+			Vector3 modelOutput = new Vector3(
+				((normData.std[0] * normalizedX) + normData.mean[0]),
+				((normData.std[1] * normalizedY) + normData.mean[1]),
+				((normData.std[2] * normalizedZ) + normData.mean[2]));
+
+			// Pequeñito
+			//Vector3 modelOutput = new Vector3(
+			//	((normData.std[0] * normalizedX) + normData.mean[0])*0.000001f,
+			//	((normData.std[1] * normalizedY) + normData.mean[1])*0.00001f,
+			//	((normData.std[2] * normalizedZ) + normData.mean[2]) * 0.000001f);
+
+			// Clamp
+			//Vector3 modelOutput = new Vector3(
+			//	Mathf.Clamp((normData.std[0] * normalizedX) + normData.mean[0], -1f, 1f),
+			//	Mathf.Clamp((normData.std[1] * normalizedY) + normData.mean[1], -1f, 1f),
+			//	Mathf.Clamp((normData.std[2] * normalizedZ) + normData.mean[2], -1f, 1f));
+
+			// Vector3 modelOutput = new Vector3(normalizedX, normalizedY, normalizedZ);
+
+			// En principio debería ser world space, pero el nuevo vértice en local space.
 			newVertices[i] = transform.InverseTransformPoint(modelOutput);
+			// newVertices[i] = modelOutput;
 			vertex = vertex + newVertices[i].ToString() + "\n";
 		}
 		SaveData(vertex);
@@ -204,27 +257,7 @@ public class ClothML : MonoBehaviour
 		result.Dispose();
 	}
 
-	Vector3 GetVelocity(int i)
-    {
-        return Vector3.zero;
-    }
-
-    float GetSDF(Vector3 pos)
-    {
-        return 0f;
-    }
-
-    float GetMaxDistance(int i)
-    {
-        return maxDistance[i];
-    }
-
-    Vector2 GetUV(int i, Mesh mesh)
-    {
-        return mesh.uv[i];
-    }
-
-    void OnDestroy()
+	void OnDestroy()
     {
         worker?.Dispose();
     }
