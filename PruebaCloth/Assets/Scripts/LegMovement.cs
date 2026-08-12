816b262719f41b83ee01b1a021e77ddf48ecc90f
efeliua <100871802+efeliua@users.noreply.github.com>
2026-08-12 11:52:26 +0200

    Cosas que no subí ups parte I

--- PruebaCloth/Assets/Scripts/LegMovement.cs
+++ PruebaCloth/Assets/Scripts/LegMovement.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Splines;
 using Vector3 = UnityEngine.Vector3;
@@ -14,6 +15,10 @@ public class LegMovement : MonoBehaviour
     private float maxRot = 30.0f;
     private Vector3 dir = Vector3.forward;
 
+    [SerializeField]
+    private float startTime = 0.0f;
+    private bool move = false;
+
     private void Start()
     {
         if (isLeft) dir = Vector3.forward;
@@ -22,11 +27,19 @@ public class LegMovement : MonoBehaviour
 
     void Update()
     {
-        if (transform.eulerAngles.z > maxRot && transform.eulerAngles.z < 180)
-		    dir = Vector3.back; 
-		else if(transform.eulerAngles.z < 330 && transform.eulerAngles.z >= 180)
-            dir = Vector3.forward; 
+        if (move)
+        {
+            if (transform.eulerAngles.z > maxRot && transform.eulerAngles.z < 180)
+                dir = Vector3.back;
+            else if (transform.eulerAngles.z < 330 && transform.eulerAngles.z >= 180)
+                dir = Vector3.forward;
 
-        transform.Rotate(dir * Time.deltaTime * movementSpeed);
+            transform.Rotate(dir * Time.deltaTime * movementSpeed);
+        }
+        else
+        {
+            startTime -= Time.deltaTime;
+            if (startTime - Time.deltaTime <= 0) move = true;
+        }
     }
 }
